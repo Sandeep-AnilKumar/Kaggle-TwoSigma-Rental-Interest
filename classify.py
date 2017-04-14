4523b1448001774589fd4c84c1eaab24da58b274
Sandeep Anil Kumar <sandeepa@yahoo-inc.com>
2017-04-13 22:51:23 -0700

    Added Grid Search

--- classify.py
+++ classify.py
@@ -1,104 +1,174 @@
 import pandas as pd
 import time
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import VotingClassifier
-from sklearn import linear_model
-from sklearn import svm
 import csv
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.ensemble import GradientBoostingClassifier as gbc
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier as rfc
 import numpy as np
+from nltk.stem import PorterStemmer
+import re
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 
 np.seterr(divide='ignore', invalid='ignore')
+stemmer = PorterStemmer()
+
+features = ['garag', 'cinema', 'hardwood', 'air condit', 'modern', 'share', 'dog', 'landlord', 'unassign', '24 hour',
+            'fireplac', 'onsit', 'cat', 'storag', 'spaciou', 'huge', 'privat', 'fee', 'yoga', 'fit', 'pet', 'open',
+            'dishwash', 'walk', 'attach', 'state', 'includ', 'inunit', 'new', 'washer', 'gym', 'deposit', 'AC', 'queen',
+            'private terrac', 'twentyfour', 'attend', 'underpr', 'brand', 'upon', 'valet', 'free', 'courtyard', 'dryer',
+            'laundri', 'ok', 'granit', 'overs', 'pool', 'avail', 'convert', 'onli', 'central', 'gift', 'wifi', 'renov',
+            'air', 'walkin', 'duplex', 'common', 'elev', 'unknown']
 
 start_time = time.time()
 train_df_total = pd.read_json('train.json')
+test_df = pd.read_json('test.json')
+for feature in features:
+    train_df_total[feature] = 0
 
-# train_high = train_df.loc[train_df['interest_level'] == 'high'].sample(frac=1)
-# train_medium = train_df.loc[train_df['interest_level'] == 'medium'].sample(frac=0.34)
-# train_low = train_df.loc[train_df['interest_level'] == 'low'].sample(frac=0.115)
-#
-# frames = [train_high, train_medium, train_low]
-#
-# train_df_total = pd.concat(frames, ignore_index=True)
+def add_features(fe):
+    found = False
+    f_list = train_df_total.loc[fe, "features"]
+    if f_list:
+        for f in f_list:
+            #print f
+            #f = f.encode('utf-8').strip()
+            for f_word in f.split(" "):
+                #print f_word
+                f_word = f_word.lower().strip()
+                f_word = f_word.replace("\"", "")
+                f_word = f_word.replace("'", "")
+                f_word = re.sub(r'(#\*!,\.\-)*', "", f_word)
+                for fea in features:
+                    if stemmer.stem(f_word) in fea:
+                        #print "found " + str(f_word) + " in " + str(fea)
+                        train_df_total.loc[fe, fea] = 1
+                        found = True
+                        break
+                if found:
+                    break
+    return
 
-print "Number of high interest houses := " + str(len(train_df_total.loc[train_df_total['interest_level'] == 'high']))
-print "Number of medium interest houses := " + str(len(train_df_total.loc[train_df_total['interest_level'] == 'medium']))
-print "Number of low interest houses := " + str(len(train_df_total.loc[train_df_total['interest_level'] == 'low']))
+def add_test_features(fe):
+    found = False
+    f_list = test_df.loc[fe, "features"]
+    if f_list:
+        for f in f_list:
+            #print f
+            #f = f.encode('utf-8').strip()
+            for f_word in f.split(" "):
+                #print f_word
+                f_word = f_word.lower().strip()
+                f_word = f_word.replace("\"", "")
+                f_word = f_word.replace("'", "")
+                f_word = re.sub(r'(#\*!,\.\-)*', "", f_word)
+                for fea in features:
+                    if stemmer.stem(f_word) in fea:
+                        #print "found " + str(f_word) + " in " + str(fea)
+                        test_df.loc[fe, fea] = 1
+                        found = True
+                        break
+                if found:
+                    break
+    return
+
+fea_list = train_df_total.index.values.tolist()
+
+#print train_df_total.loc[10000, "features"]
+for fea_l in fea_list:
+    #print fea_l
+    add_features(fea_l)
+
+#print train_df_total.loc[10000, ["elev", "dog", "cat", "fit", "renov"]]
 
 train_df_total['num_photos'] = train_df_total['photos'].apply(len)
 train_df_total['num_features'] = train_df_total['features'].apply(len)
 train_df_total['num_description'] = train_df_total['description'].apply(lambda x: len(x.split(" ")))
 
 train_df_vector = train_df_total.loc[:, ['bathrooms', 'bedrooms', 'num_photos', 'num_features', 'num_description',
-                                         'building_id', 'display_address', 'latitude', 'longitude', 'manager_id',
-                                         'price', 'street_address']]
+                                         'latitude', 'longitude', 'manager_id','price', 'street_address', 'garag',
+                                         'cinema', 'hardwood', 'air condit', 'modern', 'share', 'dog', 'landlord',
+                                         'unassign', '24 hour','fireplac', 'onsit', 'cat', 'storag', 'spaciou',
+                                         'huge', 'privat', 'fee', 'yoga', 'fit', 'pet', 'open','dishwash', 'walk',
+                                         'attach', 'state', 'includ', 'inunit', 'new', 'washer', 'gym', 'deposit',
+                                         'AC', 'queen','private terrac', 'twentyfour', 'attend', 'underpr', 'brand',
+                                         'upon', 'valet', 'free', 'courtyard', 'dryer','laundri', 'ok', 'granit',
+                                         'overs', 'pool', 'avail', 'convert', 'onli', 'central', 'gift', 'wifi',
+                                         'renov','air', 'walkin', 'duplex', 'common', 'elev', 'unknown', 'display_address',
+                                         'interest_level']]
 
-train_df_vector['building_id'] = train_df_vector['building_id'].astype('category')
-train_df_vector['display_address'] = train_df_vector['display_address'].astype('category')
 train_df_vector['manager_id'] = train_df_vector['manager_id'].astype('category')
 train_df_vector['street_address'] = train_df_vector['street_address'].astype('category')
+train_df_vector['display_address'] = train_df_vector['display_address'].astype('category')
+train_df_vector['interest_level'] = train_df_vector['interest_level'].astype('category')
 
 categorical_columns = train_df_vector.select_dtypes(['category']).columns
 train_df_vector[categorical_columns] = train_df_vector[categorical_columns].apply(lambda x: x.cat.codes)
 
 train_df_target = train_df_total.loc[:, 'interest_level']
+print("Done with training data")
 
-test_df = pd.read_json('test.json')
+Y = train_df_vector.pop('interest_level')
+X = train_df_vector.as_matrix()
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+#train = train_df_vector.sample(frac=0.8,random_state=200)
+#test = train_df_vector.drop(train.index)
+
+for feature in features:
+    test_df[feature] = 0
+
+fea_list = test_df.index.values.tolist()
+
+#print train_df_total.loc[10000, "features"]
+for fea_l in fea_list:
+    #print fea_l
+    add_test_features(fea_l)
+
+#print train_df_total.loc[10000, ["elev", "dog", "cat", "fit", "renov"]]
 
 test_df['num_photos'] = test_df['photos'].apply(len)
 test_df['num_features'] = test_df['features'].apply(len)
 test_df['num_description'] = test_df['description'].apply(lambda x: len(x.split(" ")))
 
 test_df_vector = test_df.loc[:, ['bathrooms', 'bedrooms', 'num_photos', 'num_features', 'num_description',
-                                 'building_id', 'display_address', 'latitude', 'longitude', 'manager_id', 'price',
-                                 'street_address']]
+                                         'latitude', 'longitude', 'manager_id','price', 'street_address', 'garag',
+                                         'cinema', 'hardwood', 'air condit', 'modern', 'share', 'dog', 'landlord',
+                                         'unassign', '24 hour','fireplac', 'onsit', 'cat', 'storag', 'spaciou',
+                                         'huge', 'privat', 'fee', 'yoga', 'fit', 'pet', 'open','dishwash', 'walk',
+                                         'attach', 'state', 'includ', 'inunit', 'new', 'washer', 'gym', 'deposit',
+                                         'AC', 'queen','private terrac', 'twentyfour', 'attend', 'underpr', 'brand',
+                                         'upon', 'valet', 'free', 'courtyard', 'dryer','laundri', 'ok', 'granit',
+                                         'overs', 'pool', 'avail', 'convert', 'onli', 'central', 'gift', 'wifi',
+                                         'renov','air', 'walkin', 'duplex', 'common', 'elev', 'unknown', 'display_address']]
 
-test_df_vector['building_id'] = test_df_vector['building_id'].astype('category')
-test_df_vector['display_address'] = test_df_vector['display_address'].astype('category')
 test_df_vector['manager_id'] = test_df_vector['manager_id'].astype('category')
 test_df_vector['street_address'] = test_df_vector['street_address'].astype('category')
+test_df_vector['display_address'] = test_df_vector['display_address'].astype('category')
 
 categorical_columns = test_df_vector.select_dtypes(['category']).columns
 test_df_vector[categorical_columns] = test_df_vector[categorical_columns].apply(lambda x: x.cat.codes)
 
 test_df_ids = list(test_df['listing_id'])
+print("Done with testing data")
 
-#Gradient Boost Classifier
-#gbc_clf = gbc(loss='deviance')
+estimator = rfc(n_estimators=1000)
+n_estimators = np.arange(10,10000,1)
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+classifier = GridSearchCV(estimator=estimator, cv=cv, param_grid=dict(n_estimators=n_estimators))
+classifier.fit(x_train, y_train)
 
-# SGDC classifier
-# sgd_clf = linear_model.SGDClassifier(loss='log')
-# #
-# # # KNN classifier
-# # knn_clf = KNeighborsClassifier()
-# #
-# #logistic regression
-# lr_clf = linear_model.LogisticRegression()
+print "best n_estimators := " + str(classifier.best_estimator_.n_estimators)
 #
-# # Naive Bayes
-# nb_clf = BernoulliNB()
-#
+rfc_clf = rfc(n_estimators=classifier.best_estimator_.n_estimators)
+rfc_clf.fit(Y, train_df_target)
 
-# ab_clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=2), n_estimators=600, learning_rate=1.5,
-#                                   algorithm="SAMME.R")
-
-# Ensemble classifier, taking best 3 classifiers out of the above 5.
-# voting_clf = VotingClassifier(estimators=[('sgd', sgd_clf), ('lr', lr_clf), ('ab_clf', ab_clf)],
-#                               weights=[0.5, 0.5, 1], voting='soft')
-
-rfc_clf = rfc(n_estimators=1000)
-
-rfc_clf.fit(train_df_vector, train_df_target)
 voting_prediction = rfc_clf.predict_proba(test_df_vector)
 
-print("Time for the Voting Classifier to train and predict on the testing data subset is := %.2f" % (time.time() -
+print("Time for the Random Forest Classifier to train and predict on the testing data is := %.2f" % (time.time() -
                                                                                                      start_time))
 
-csv_file = open("submissions.csv", 'w')
+csv_file = open("submissions_new.csv", 'w')
 wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_NONE)
 wr.writerow(['listing_id', 'high', 'medium', 'low'])
 
--- sampling.py
+++ sampling.py
@@ -3,63 +3,161 @@ import time
 import csv
 from sklearn.ensemble import RandomForestClassifier as rfc
 import numpy as np
+from nltk.stem import PorterStemmer
+import re
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 
 np.seterr(divide='ignore', invalid='ignore')
+stemmer = PorterStemmer()
+
+features = ['garag', 'cinema', 'hardwood', 'air condit', 'modern', 'share', 'dog', 'landlord', 'unassign', '24 hour',
+            'fireplac', 'onsit', 'cat', 'storag', 'spaciou', 'huge', 'privat', 'fee', 'yoga', 'fit', 'pet', 'open',
+            'dishwash', 'walk', 'attach', 'state', 'includ', 'inunit', 'new', 'washer', 'gym', 'deposit', 'AC', 'queen',
+            'private terrac', 'twentyfour', 'attend', 'underpr', 'brand', 'upon', 'valet', 'free', 'courtyard', 'dryer',
+            'laundri', 'ok', 'granit', 'overs', 'pool', 'avail', 'convert', 'onli', 'central', 'gift', 'wifi', 'renov',
+            'air', 'walkin', 'duplex', 'common', 'elev', 'unknown']
 
 start_time = time.time()
 train_df_total = pd.read_json('train.json')
+for feature in features:
+    train_df_total[feature] = 0
 
-print "Number of high interest houses := " + str(len(train_df_total.loc[train_df_total['interest_level'] == 'high']))
-print "Number of medium interest houses := " + str(len(train_df_total.loc[train_df_total['interest_level'] == 'medium']))
-print "Number of low interest houses := " + str(len(train_df_total.loc[train_df_total['interest_level'] == 'low']))
+def add_features(fe):
+    found = False
+    f_list = train_df_total.loc[fe, "features"]
+    if f_list:
+        for f in f_list:
+            #print f
+            #f = f.encode('utf-8').strip()
+            for f_word in f.split(" "):
+                #print f_word
+                f_word = f_word.lower().strip()
+                f_word = f_word.replace("\"", "")
+                f_word = f_word.replace("'", "")
+                f_word = re.sub(r'(#\*!,\.\-)*', "", f_word)
+                for fea in features:
+                    if stemmer.stem(f_word) in fea:
+                        #print "found " + str(f_word) + " in " + str(fea)
+                        train_df_total.loc[fe, fea] = 1
+                        found = True
+                        break
+                if found:
+                    break
+    return
+
+def add_test_features(fe):
+    found = False
+    f_list = test_df.loc[fe, "features"]
+    if f_list:
+        for f in f_list:
+            #print f
+            #f = f.encode('utf-8').strip()
+            for f_word in f.split(" "):
+                #print f_word
+                f_word = f_word.lower().strip()
+                f_word = f_word.replace("\"", "")
+                f_word = f_word.replace("'", "")
+                f_word = re.sub(r'(#\*!,\.\-)*', "", f_word)
+                for fea in features:
+                    if stemmer.stem(f_word) in fea:
+                        #print "found " + str(f_word) + " in " + str(fea)
+                        test_df.loc[fe, fea] = 1
+                        found = True
+                        break
+                if found:
+                    break
+    return
+
+fea_list = train_df_total.index.values.tolist()
+
+#print train_df_total.loc[10000, "features"]
+# for fea_l in fea_list:
+#     #print fea_l
+#     add_features(fea_l)
+
+#print train_df_total.loc[10000, ["elev", "dog", "cat", "fit", "renov"]]
 
 train_df_total['num_photos'] = train_df_total['photos'].apply(len)
 train_df_total['num_features'] = train_df_total['features'].apply(len)
 train_df_total['num_description'] = train_df_total['description'].apply(lambda x: len(x.split(" ")))
 
 train_df_vector = train_df_total.loc[:, ['bathrooms', 'bedrooms', 'num_photos', 'num_features', 'num_description',
-                                         'building_id', 'display_address', 'latitude', 'longitude', 'manager_id',
-                                         'price', 'street_address']]
+                                         'latitude', 'longitude', 'manager_id','price', 'street_address', 'garag',
+                                         'cinema', 'hardwood', 'air condit', 'modern', 'share', 'dog', 'landlord',
+                                         'unassign', '24 hour','fireplac', 'onsit', 'cat', 'storag', 'spaciou',
+                                         'huge', 'privat', 'fee', 'yoga', 'fit', 'pet', 'open','dishwash', 'walk',
+                                         'attach', 'state', 'includ', 'inunit', 'new', 'washer', 'gym', 'deposit',
+                                         'AC', 'queen','private terrac', 'twentyfour', 'attend', 'underpr', 'brand',
+                                         'upon', 'valet', 'free', 'courtyard', 'dryer','laundri', 'ok', 'granit',
+                                         'overs', 'pool', 'avail', 'convert', 'onli', 'central', 'gift', 'wifi',
+                                         'renov','air', 'walkin', 'duplex', 'common', 'elev', 'unknown', 'display_address']]
 
-train_df_vector['building_id'] = train_df_vector['building_id'].astype('category')
-train_df_vector['display_address'] = train_df_vector['display_address'].astype('category')
 train_df_vector['manager_id'] = train_df_vector['manager_id'].astype('category')
 train_df_vector['street_address'] = train_df_vector['street_address'].astype('category')
+train_df_vector['display_address'] = train_df_vector['display_address'].astype('category')
 
 categorical_columns = train_df_vector.select_dtypes(['category']).columns
 train_df_vector[categorical_columns] = train_df_vector[categorical_columns].apply(lambda x: x.cat.codes)
 
 train_df_target = train_df_total.loc[:, 'interest_level']
+print("Done with training data")
+
+#X_train, X_test, y_train, y_test = train_test_split(train_df_total, train_df_target, test_size=0.2, random_state=0)
+train = train_df_vector.sample(frac=0.8,random_state=200)
+test = train_df_vector.drop(train.index)
 
 test_df = pd.read_json('test.json')
+for feature in features:
+    test_df[feature] = 0
+
+fea_list = test_df.index.values.tolist()
+
+#print train_df_total.loc[10000, "features"]
+# for fea_l in fea_list:
+#     #print fea_l
+#     add_test_features(fea_l)
+
+#print train_df_total.loc[10000, ["elev", "dog", "cat", "fit", "renov"]]
 
 test_df['num_photos'] = test_df['photos'].apply(len)
 test_df['num_features'] = test_df['features'].apply(len)
 test_df['num_description'] = test_df['description'].apply(lambda x: len(x.split(" ")))
 
 test_df_vector = test_df.loc[:, ['bathrooms', 'bedrooms', 'num_photos', 'num_features', 'num_description',
-                                 'building_id', 'display_address', 'latitude', 'longitude', 'manager_id', 'price',
-                                 'street_address']]
+                                         'latitude', 'longitude', 'manager_id','price', 'street_address', 'garag',
+                                         'cinema', 'hardwood', 'air condit', 'modern', 'share', 'dog', 'landlord',
+                                         'unassign', '24 hour','fireplac', 'onsit', 'cat', 'storag', 'spaciou',
+                                         'huge', 'privat', 'fee', 'yoga', 'fit', 'pet', 'open','dishwash', 'walk',
+                                         'attach', 'state', 'includ', 'inunit', 'new', 'washer', 'gym', 'deposit',
+                                         'AC', 'queen','private terrac', 'twentyfour', 'attend', 'underpr', 'brand',
+                                         'upon', 'valet', 'free', 'courtyard', 'dryer','laundri', 'ok', 'granit',
+                                         'overs', 'pool', 'avail', 'convert', 'onli', 'central', 'gift', 'wifi',
+                                         'renov','air', 'walkin', 'duplex', 'common', 'elev', 'unknown', 'display_address']]
 
-test_df_vector['building_id'] = test_df_vector['building_id'].astype('category')
-test_df_vector['display_address'] = test_df_vector['display_address'].astype('category')
 test_df_vector['manager_id'] = test_df_vector['manager_id'].astype('category')
 test_df_vector['street_address'] = test_df_vector['street_address'].astype('category')
+test_df_vector['display_address'] = test_df_vector['display_address'].astype('category')
 
 categorical_columns = test_df_vector.select_dtypes(['category']).columns
 test_df_vector[categorical_columns] = test_df_vector[categorical_columns].apply(lambda x: x.cat.codes)
 
 test_df_ids = list(test_df['listing_id'])
+print("Done with testing data")
 
-rfc_clf = rfc(n_estimators=1000)
+estimator = rfc(n_estimators=1000)
+gammas = np.logspace(-6, -1, 10)
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+classifier = GridSearchCV(estimator=estimator, cv=cv, param_grid=dict(gamma=gammas))
+classifier.fit(train_df_vector, train_df_target)
 
-rfc_clf.fit(train_df_vector, train_df_target)
-voting_prediction = rfc_clf.predict_proba(test_df_vector)
+voting_prediction = classifier.predict_proba(test_df_vector)
 
 print("Time for the Random Forest Classifier to train and predict on the testing data is := %.2f" % (time.time() -
                                                                                                      start_time))
 
-csv_file = open("submissions.csv", 'w')
+csv_file = open("submissions_new.csv", 'w')
 wr = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_NONE)
 wr.writerow(['listing_id', 'high', 'medium', 'low'])
 
